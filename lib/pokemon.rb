5c6c590169a182474112842371b6996de5f042ed
Simran <mdrsimran@gmail.com>
2020-11-20 10:49:28 -0600

    commit tree 145a608dc79597b5d29a8b83593cae2e80902d61

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
 class Pokemon
+   
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+        db.execute(sql, name , type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT *
+        FROM pokemon
+        WHERE id = ?
+        LIMIT 1
+        SQL
+        poke = db.execute(sql, id)[0]
+        Pokemon.new(id: poke[0], name: poke[1], type: poke[2], db: db)
+    end
+
+    # def self.find(id, db)
+    #     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
+    #     Pokemon.new(id: poke[0], name: poke[1], type: poke[2], db: db)
+   
+    # end
 end
